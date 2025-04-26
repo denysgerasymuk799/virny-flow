fee7c1ea752e37f816b0d7acb8329999c64d73f2
Denys Herasymuk <herasymuk@ucu.edu.ua>
2025-04-26 22:46:14 +0300

    Checked heart

--- virny_flow_demo/configs/datasets_config.py
+++ virny_flow_demo/configs/datasets_config.py
@@ -1,4 +1,4 @@
-from virny.datasets import DiabetesDataset2019, ACSEmploymentDataset, ACSPublicCoverageDataset
+from virny.datasets import DiabetesDataset2019, ACSEmploymentDataset, ACSPublicCoverageDataset, CardiovascularDiseaseDataset
 from virny_flow_demo.configs.data_loaders import GermanCreditDataset
 
 SEED = 42
@@ -24,6 +24,11 @@ DATASET_CONFIG = {
         "data_loader": ACSPublicCoverageDataset,
         "data_loader_kwargs": {"state": ['NY'], "year": 2018, "with_nulls": False,
                                "subsample_size": 50_000, "subsample_seed": SEED},
+        "test_set_fraction": 0.2,
+    },
+    "heart": {
+        "data_loader": CardiovascularDiseaseDataset,
+        "data_loader_kwargs": {},
         "test_set_fraction": 0.2,
     },
 }
